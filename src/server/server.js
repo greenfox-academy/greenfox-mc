17172b663e4ca26952a1d46c7c4429f713b7c07d
Dima Lambrianov <dmitry.lambrianov@kinja.com>
2017-01-31 16:05:48 +0100

    feat: add express middleware to count requests

--- src/server/server.js
+++ src/server/server.js
@@ -5,9 +5,16 @@ require("babel-core/register");
 require("babel-polyfill");
 const express = require('express');
 const app = express();
+const monitor = di.container.get('monitor');
 
 const bodyParser = require('body-parser')
 app.use(bodyParser.json());
+
+app.use(function (req, res, next) {
+  monitor.registerIncomingRequest(req.url, req.param, Date.now());
+  console.log(monitor.getStatistic().totalIncomingRequests);
+  next()
+})
 
 app.use('/', express.static(path.join(__dirname, process.env.PUBLIC_DIR)))
 
